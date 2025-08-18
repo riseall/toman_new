dc4fe76324e3f2558f13ac455d89b76aef21feeb
Rizal Nugroho <rizalnug2@gmail.com>
2025-08-18 09:21:19 +0700

    feat: change the user and add company migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,55 +16,8 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'username',
-        'first_name',
-        'last_name',
-        'email',
-        'password',
-        'phone',
-        'address_line_1',
-        'address_line_2',
-        'negara',
-        'provinsi',
-        'kota',
-        'kecamatan',
-        'kelurahan',
-        'rt',
-        'rw',
-        'postal_code',
-        'about',
-        'gender',
-        'photo',
-        'image',
-        'is_admin',
-        'entity_code',
-        'entity_id',
-        'entity_name',
-        'is_toller',
-        'is_maklooner',
-        'user_last_logon',
-        'user__char_01',
-        'user__int_01',
-        'user__decimal_01',
-        'user__date_01',
-        'is_active',
-        'is_accept',
-        'is_agree',
-        'is_agree_01',
-        'is_agree_02',
-        'is_agree_03',
-        'is_member',
-        'is_suspended',
-        'is_confirmed',
-        'is_verified',
-        'is_logged_in',
-        'is_logged_out',
-        'login_time',
-        'logout_time',
-        'accepted',
-        'rejected',
-        'remembered',
+    protected $guarded = [
+        'id'
     ];
 
     /**
@@ -74,7 +26,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
@@ -83,28 +36,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'user_last_logon' => 'datetime',
-        'user__date_01' => 'datetime',
-        'login_time' => 'datetime',
-        'logout_time' => 'datetime',
-        'is_admin' => 'boolean',
-        'is_toller' => 'boolean',
-        'is_maklooner' => 'boolean',
         'is_active' => 'boolean',
-        'is_accept' => 'boolean',
-        'is_agree' => 'boolean',
-        'is_agree_01' => 'boolean',
-        'is_agree_02' => 'boolean',
-        'is_agree_03' => 'boolean',
-        'is_member' => 'boolean',
-        'is_suspended' => 'boolean',
-        'is_confirmed' => 'boolean',
-        'is_verified' => 'boolean',
-        'is_logged_in' => 'boolean',
-        'is_logged_out' => 'boolean',
-        'accepted' => 'boolean',
-        'rejected' => 'boolean',
-        'remembered' => 'boolean',
     ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
