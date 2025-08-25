50255b9160fe83ef6e722ea85975c4610f842b08
Rizal Nugroho <rizalnug2@gmail.com>
2025-08-25 08:30:12 +0700

    feat: add welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\FasilitasProduksiController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PdfController;
 use App\Http\Controllers\PermintaanController;
@@ -18,6 +19,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route Beranda
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
@@ -26,7 +32,17 @@ Route::group(['middleware' => ['role:super_admin']], function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 });
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Route Layanan
+Route::get('/layanan/toll_murni', [FasilitasProduksiController::class, 'index'])->name('toll_murni');
+
+Route::get('/layanan/toll_beli', function () {
+    return view('layanan.toll_beli');
+})->name('toll_beli');
+
+Route::get('/layanan/kalibrasi', function () {
+    return view('layanan.kalibrasi');
+})->name('kalibrasi');
+
 
 // Route Untuk Admin (User)
 Route::resource('user', 'App\Http\Controllers\UserController');
@@ -40,6 +56,7 @@ Route::delete('/product/{id}', [ProductController::class, 'deleteProduct'])->nam
 // Route::get('/permintaan', [PermintaanController::class, 'showReq'])->name('permintaan.show');
 // Route::post('/permintaan/add', [PermintaanController::class, 'storeReq'])->name('permintaan.add');
 Route::resource('permintaan', PermintaanController::class)->name('permintaan', 'permintaan');
+
 // Route untuk export PDF
 Route::get('/permintaan/{id}/pdf', [PdfController::class, 'exportPdf'])->name('permintaan.export_pdf');
 
