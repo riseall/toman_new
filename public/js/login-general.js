cb167266c9354eaee2d6f4559c36cd6cc7dfe7ed
Rizal Nugroho <rizalnug2@gmail.com>
2025-10-01 16:25:13 +0700

    fix: route forgot password

--- public/js/login-general.js
+++ public/js/login-general.js
@@ -97,7 +97,7 @@ var KTLogin = (function () {
             let token = $("#kt_login_forgot_form input[name='_token']").val();
 
             $.ajax({
-                url: "/forgot-password", // sesuai route('password.email')
+                url: "{{ route('password.email') }}", // sesuai route('password.email')
                 type: "POST",
                 data: {
                     _token: token,
