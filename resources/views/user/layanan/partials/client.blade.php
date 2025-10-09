f7721de0019706acd1cbd61a0aa87e2a9ca10e68
Rizal Nugroho <rizalnug2@gmail.com>
2025-10-09 15:06:01 +0700

    feat: penyesuaian logo partner

--- resources/views/user/layanan/partials/client.blade.php
+++ resources/views/user/layanan/partials/client.blade.php
@@ -20,6 +20,7 @@
              <img class="client-logo" src="{{ asset('images/partner/pyridam.png') }}" alt="PT. Pyridam Farma">
              <img class="client-logo" src="{{ asset('images/partner/kimiafarma.png') }}" alt="PT. Kimia Farma">
              <img class="client-logo" src="{{ asset('images/partner/erlimpex.png') }}" alt="PT. Erlimpex">
+             <img class="client-logo" src="{{ asset('images/partner/combiphar.png') }}" alt="PT. Combiphar">
              <img class="client-logo" src="{{ asset('images/partner/MF.png') }}" alt="PT. Metiska Farma">
              <img class="client-logo" src="{{ asset('images/partner/nulab.png') }}"
                  alt="PT. Nulab Pharmaceutical Indonesia">
@@ -30,15 +31,15 @@
                  alt="PT. Ferron Par Pharmaceutical">
              <img class="client-logo" src="{{ asset('images/partner/mbf.png') }}" alt="PT. Mahakam Beta Farma">
              <img class="client-logo" src="{{ asset('images/partner/marin.png') }}" alt="PT. Marin Liza">
-             {{-- <img class="client-logo" src="{{ asset('images/partner/') }}" alt="PT. Wigo Health Indonesia"> --}}
+             <img class="client-logo" src="{{ asset('images/partner/wigo.png') }}" alt="PT. Wigo Health Indonesia">
              <img class="client-logo" src="{{ asset('images/partner/lapi.png') }}" alt="PT. Lapi Laboratories">
              <img class="client-logo" src="{{ asset('images/partner/ipha.png') }}" alt="PT. IPHA Laboratories">
              <img class="client-logo" src="{{ asset('images/partner/gmp.png') }}" alt="PT. Global Multi Pharmalab">
              <img class="client-logo" src="{{ asset('images/partner/gracia.png') }}" alt="Gracia Pharmindo">
-             {{-- <img class="client-logo" src="{{ asset('images/partner/molex.png') }}" alt="PT. Molex Ayus"> --}}
-             {{-- <img class="client-logo" src="{{ asset('images/partner/yarindo.png') }}" alt="PT. Yarindo Farmatama"> --}}
+             <img class="client-logo" src="{{ asset('images/partner/molex.png') }}" alt="PT. Molex Ayus">
+             <img class="client-logo" src="{{ asset('images/partner/yarindo.png') }}" alt="PT. Yarindo Farmatama">
              <img class="client-logo" src="{{ asset('images/partner/soho.png') }}" alt="PT. Soho Industri Farmasi">
-             {{-- <img class="client-logo" src="{{ asset('images/partner/sunthi.png') }}" alt="PT. Sunthi Sepuri"> --}}
+             <img class="client-logo" src="{{ asset('images/partner/sunthi.png') }}" alt="PT. Sunthi Sepuri">
              {{-- <img class="client-logo" src="{{ asset('images/partner/braun.png') }}" alt="Badan Pom"> --}}
          </div>
      </div>
