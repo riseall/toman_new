77f32abeaf4736163041e634f2a4856d96c4c845
Rizal Nugroho <rizalnug2@gmail.com>
2025-10-17 08:27:25 +0700

    feat: add page maintenace

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta charset="utf-8">
+    <title>Toman - Error</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Toll Manufakturing Pt. Phapros.tbk">
+    <meta name="keywords" content="Jasa toll manufacturing [obat, tablet, kapsul], Manufaktur toll, Phapros, Pabrik toll">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="{{ asset('images/logo/toman.png') }}">
+
+    <!-- Css -->
+    <!-- Icons Css -->
+    <link href="{{ asset('css/bootstrap.css') }}" id="bootstrap-style" class="theme-opt" rel="stylesheet"
+        type="text/css">
+    <!-- Style Css-->
+    <link href="{{ asset('css/style.css') }}" id="color-opt" class="theme-opt" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <!-- ERROR PAGE -->
+    <section class="bg-home d-flex align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-md-12 text-center">
+                    <img src="{{ asset('images/503.gif') }}" class="img-fluid" alt="">
+                </div><!--end col-->
+            </div><!--end row-->
+        </div><!--end container-->
+    </section><!--end section-->
+    <!-- ERROR PAGE -->
+</body>
+
+</html>
